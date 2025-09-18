570cbd7a69e5e0c1075ddbc28a86aa650a5d4760
PTVux <kidkaito680@gmail.com>
2025-09-18 14:01:42 +0700

    Add project files.

---
+++ BT1/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BT1.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace BT1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) :
+            base(dbContextOptions)
+        {
+            //constructor
+        }
+        //define C# model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // có thể định nghĩa moi quan he giua cac table bang Fluent API
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(bi => bi.BookId);
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(b => b.Authors)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(bi => bi.AuthorId);
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Book_Author> Books_Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
---
+++ BT1/Models/Domain/Book_Author.cs
@@ -0,0 +1,11 @@
+﻿namespace BT1.Models.Domain
+{
+    public class Book_Author
+    {
+        public int Id { get; set; } // Primary key
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int AuthorId { get; set; }
+        public Authors Authors { get; set; }
+    }
+}
